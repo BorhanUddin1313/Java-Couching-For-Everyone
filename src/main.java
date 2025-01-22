eaccabeeb88637316f5b01e53ee4a9480fb73101
Uddin <hvr193@usask.ca>
2025-01-22 16:21:02 -0600

    first commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Whats up dwag: ");
+        String sup= sc.nextLine();
+        System.out.print("MY man!!!! "+ sup);
+
+    }
+}
