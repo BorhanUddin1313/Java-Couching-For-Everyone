7b288ee80c26f5a126aa39adfe2b09fba3c4be1d
Uddin <hvr193@usask.ca>
2025-01-22 17:18:44 -0600

    leetcode

--- src/main.java
+++ src/main.java
@@ -2,10 +2,31 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        System.out.print("What is the string: ");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Whats up dwag: ");
-        String sup= sc.nextLine();
-        System.out.print("MY man!!!! "+ sup);
+        String s = sc.nextLine();
+        if (check(s)){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
+    public static boolean check(String s) {
+        boolean hasB = false;
+        for (int i = 0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c=='a' && hasB){
+                return false;
+            }
+            if (c=='b'){
+                hasB=true;
+            }
+
+        }
+        return true;
 
     }
+
 }
+
+
